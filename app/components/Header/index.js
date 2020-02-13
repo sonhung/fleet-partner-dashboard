5944646d1838a481ac8b20037d96aaa44e45cd31
sonhung <sonhung.nue@gmail.com>
2020-02-13 10:03:04 +0700

    create config file

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -1,18 +1,37 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
 import { FormattedMessage } from 'react-intl';
 import LocaleToggle from 'containers/LocaleToggle';
 import messages from './messages';
 
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 8px 30px;
+`
+
+const Menu = styled.div`
+  a {
+    margin: 0 10px;
+  }
+`
+
 function Header() {
   return (
-    <div>
-      <section>
+    <Container>
+      <div>
         <FormattedMessage {...messages.title} />
-      </section>
-      <section>
+      </div>
+      <Menu>
+        <Link to="/">Home page</Link>
+        <Link to="/users">List user</Link>
+      </Menu>
+      <div>
         <LocaleToggle />
-      </section>
-    </div>
+      </div>
+    </Container>
   );
 }
 
